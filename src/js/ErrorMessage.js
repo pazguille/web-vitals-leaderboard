9192b2a347f9123e970c3466278e012107cb6455
pazguille <pazguille@users.noreply.github.com>
2020-11-28 17:18:15 +0000

    Deploying to gh-pages from  @ 53cad727f647f3aa6e5cdab395d7521d52bb4ba7 🚀

--- src/js/ErrorMessage.js
+++
@@ -1,1 +0,0 @@
-import store from"./store.js";export default class ErrorMessage{constructor(){this.$node=document.querySelector("#error-message")}toggleVisibility(){const{leaderboardError:e,leaderboardLoaded:t}=store.getState();e?this.$node.removeAttribute("hidden"):t||this.$node.setAttribute("hidden","hidden")}init(){store.subscribe(this.toggleVisibility.bind(this))}}
--- src/js/Loading.js
+++
@@ -1,1 +0,0 @@
-import store from"./store.js";export default class Loading{constructor(){this.$node=document.querySelector("#loading")}toggleVisibility(){const{running:t,leaderboardLoaded:e}=store.getState();t?this.$node.removeAttribute("hidden"):e&&this.$node.setAttribute("hidden","hidden")}init(){store.subscribe(this.toggleVisibility.bind(this))}}
